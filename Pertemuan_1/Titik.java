c2c4443e9504515f6365465d3815b6a7762e419c
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-02-22 19:42:48 +0700

    Inisialisasi repo

---
+++ Pertemuan_1/Titik.java
@@ -0,0 +1,37 @@
+public class Titik {
+	double absis;
+	double ordinat;
+	static int counterTitik; //menghitung objek titik
+	
+	Titik(double a, double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+	
+	Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+	
+	void setAbsis(double a){
+		absis = a;
+	}
+	
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+	
+	double getAbsis(){
+		return absis;
+	}
+	
+	double getOrdinat(){
+		return ordinat;
+	}
+	
+	static int getCounterTitik(){
+		return counterTitik;
+	}
+}
